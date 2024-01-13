38872c4ad1a98a8a5e99e4d8ccc8ae9610b2407c
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-01-13 23:41:55 +0300

    QR Code generator

---
+++ generate_code.py
@@ -0,0 +1,19 @@
+# pip install qrcode
+
+import qrcode
+from PIL import Image
+
+# The data you want to encode in the QR Code
+data = "https://x.com"
+
+# Generate the QR Code
+qr = qrcode.QRCode(version=1, box_size=10, border=5)
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code
+image = qr.make_image(fill="black", back_color="white")
+
+# Save the image
+image.save("qr_code.png")
+Image.open("qr_code.png")
