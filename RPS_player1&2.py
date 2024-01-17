20fc77f3ab5f57ce83822f0f504b28fb29e49b90
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-01-17 19:49:54 +0300

    Update RPS_player1&2.py
    syntax fixes

--- RPS_player1&2.py
+++ RPS_player1&2.py
@@ -1,16 +1,14 @@
-# Two player, Rock/Paper/Scissors
-
 user1_wins = 0
 user2_wins = 0
 draws = 0
 
-Player_1 = input("What is you name player 1?  ")
-Player_2 = input("What is your name player 2?  ")
+Player_1 = input("What is your name, player 1? ")
+Player_2 = input("What is your name, player 2? ")
 
 options = ["rock", "paper", "scissors"]
 
 while True:
-    user1_input = input(Player_1,( "type Rock/Paper/Scissors or Q to quit: ")).lower()
+    user1_input = input("{} type Rock/Paper/Scissors or Q to quit: ".format(Player_1)).lower()
     if user1_input == "q":
         break
 
@@ -18,7 +16,7 @@ while True:
         print("Invalid input. Please try again.")
         continue
 
-    user2_input = input(Player_2, "type Rock/Paper/Scissors or Q to quit: ").lower()
+    user2_input = input("{} type Rock/Paper/Scissors or Q to quit: ".format(Player_2)).lower()
     print()
     if user2_input == "q":
         break
@@ -27,8 +25,8 @@ while True:
         print("Invalid input. Please try again.")
         continue
 
-    print(Player_1, "picked", user1_input + ".")
-    print(Player_2, "picked", user2_input + ".")
+    print("{} picked {}".format(Player_1, user1_input))
+    print("{} picked {}".format(Player_2, user2_input))
     print()
 
     if (
@@ -36,27 +34,27 @@ while True:
         or (user1_input == "paper" and user2_input == "rock")
         or (user1_input == "scissors" and user2_input == "paper")
     ):
-        print(Player_1, "wins!")
+        user1_wins += 1
+        print("{} wins{}!".format(Player_1, "" if user1_wins == 1 else " {} times".format(user1_wins)))
         print("Go again!!")
         print()
-        user1_wins += 1
 
     elif (
         (user2_input == "rock" and user1_input == "scissors")
         or (user2_input == "paper" and user1_input == "rock")
         or (user2_input == "scissors" and user1_input == "paper")
     ):
-        print(Player_2, "wins!")
+        user2_wins += 1
+        print("{} wins{}!".format(Player_2, "" if user2_wins == 1 else " {} times".format(user2_wins)))
         print("Go again!!")
         print()
-        user2_wins += 1
 
     else:
+        draws += 1
         print("It's a tie!")
         print()
-        draws += 1
 
-print(Player_1, "won", user1_wins, "times.")
-print(Player_2, "won", user2_wins, "times.")
-print("The game was tied", draws ,"times.")
+print("{} won {}.".format(Player_1, "1 time" if user1_wins == 1 else "{} times".format(user1_wins)))
+print("{} won {}.".format(Player_2, "1 time" if user2_wins == 1 else "{} times".format(user2_wins)))
+print("The game was tied {}.".format("1 time" if draws == 1 else "{} times".format(draws)))
 print("Goodbye!")
