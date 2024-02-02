af96cbfbe06a37eb53df378cc1a8854a36b57ab6
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-02-03 01:03:18 +0300

    tests

--- generate_code.py
+++ generate_code.py
@@ -4,7 +4,7 @@ import qrcode
 from PIL import Image
 
 # The data you want to encode in the QR Code
-data = "https://github.com/TrevorMwangi"
+data = "http://foodlink.unaux.com/"
 
 # Generate the QR Code
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
@@ -13,6 +13,7 @@ qr.make(fit=True)
 
 # Create an image from the QR Code
 image = qr.make_image(fill="black", back_color="white")
+print("QR Code generated!!")
 
 # Save the image
 image.save("qr_code.png")
