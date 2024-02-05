c22c918c296e64d1ffb0a7818101c6409c662a76
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-02-05 23:50:45 +0300

    Update zipcode.py

--- zipcode.py
+++ zipcode.py
@@ -1,1 +1,10 @@
 # pip install geopy
+
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="geoapiExercises")
+
+place = input("Enter city name:  ")
+
+location = geolocator.geocode(place)
+print(location)
