30e7f273bf75c198c371ef347a101ff4ee05d9b8
Trevor <trvrmwangi@gmail.com>
2024-02-12 11:17:21 +0300

    Update get_country_info.py
    try and except block

--- get_country_info.py
+++ get_country_info.py
@@ -2,10 +2,15 @@
 
 from countryinfo import CountryInfo
 
-count = input("Enter your country: ")
-country = CountryInfo(count)
-print("Capital is : ",country.capital())
-print("Currency is : ",country.currencies())
-print("Language is : ",country.languages())
-print("Bordering countries are : ",country.borders())
-print("Other names : ",country.alt_spellings())
+try:
+    count = input("Enter your country: ")
+    country = CountryInfo(count)
+    print("Capital is:", country.capital())
+    print("Currency is:", country.currencies())
+    print("Language is:", country.languages())
+    print("Bordering countries are:", country.borders())
+    print("Other names:", country.alt_spellings())
+except KeyError:
+    print("Sorry, that is not a recognized country.")
+except Exception as e:
+    print("An error occurred:", str(e))
