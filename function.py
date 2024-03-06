ee9bd13ab2a14666059b7d377f627f82dddf08fd
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-03-06 23:28:51 +0300

    Update function.py
    memory_profiler

--- function.py
+++ function.py
@@ -1,6 +1,7 @@
-def fun(x, y):
-    if x == 0:
-        return y
-    else:
-        return fun(x - 1, x * y)
-print(fun(4,2))
+from memory_profiler import profile
+
+@profile
+def my_function():
+    #your code here
+
+    my_function()
