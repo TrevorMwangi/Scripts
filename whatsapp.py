4a1836edac3aaf04689fb3ba0162c016630a2f97
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-05-01 23:23:01 +0300

    Update whatsapp.py
    troublesome

--- whatsapp.py
+++ whatsapp.py
@@ -3,16 +3,15 @@ from datetime import datetime, timedelta
 
 # Calculate the current time plus a short delay
 current_time = datetime.now()
-send_time = current_time + timedelta(seconds=20)
 
 # Extract hour and minute from the send_time
-hour = send_time.hour
-minute = send_time.minute
+hour = current_time.hour
+minute = current_time.minute
 
 # Attempt to send a Whatsapp message
 
 try:
-	pywhatkit.sendwhatmsg("+254115251183","Hello there!", hour, minute)
+	pywhatkit.sendwhatmsg("+254115251183","Hello there!", hour, minute, wait_time=15)
 	print("Successfully sent!")
 	
 except Exception as e:
