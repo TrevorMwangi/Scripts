00371e260d2d0cc6a73f59716ac6593f01b1fe78
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-05-01 23:08:04 +0300

    Update whatsapp.py
    method to calculate the current time plus a short delay

--- whatsapp.py
+++ whatsapp.py
@@ -1,12 +1,22 @@
 import pywhatkit
+from datetime import datetime, timedelta
 
+# Calculate the current time plus a short delay
+current_time = datetime.now()
+send_time = current_time + timedelta(seconds=20)
+
+# Extract hour and minute from the send_time
+hour = send_time.hour
+minute = send_time.minute
+
+# Attempt to send a Whatsapp message
 
 try:
-	pywhatkit.sendwhatmsg("+254115251183","Hello there!",23, 11)
+	pywhatkit.sendwhatmsg("+254115251183","Hello there!", hour, minute)
 	print("Successfully sent!")
 	
-except:
-	print("An unexpected error!")
+except Exception as e:
+	print("An unexpected error!", e)
 
 # send messages with python
 # pip install pywhatkit
