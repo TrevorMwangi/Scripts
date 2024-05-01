ce5cfc2b2e56dbba9795a1ca9cc6d2b45b838da9
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-05-01 22:59:39 +0300

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,8 +1,12 @@
 import pywhatkit
 
+
 try:
-	pywhatkit.sendwhatmsg("+254115251183","Hello there!",21, 23)
+	pywhatkit.sendwhatmsg("+254115251183","Hello there!",23, 11)
 	print("Successfully sent!")
 	
 except:
 	print("An unexpected error!")
+
+# send messages with python
+# pip install pywhatkit
